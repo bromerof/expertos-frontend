e48453d42501ddf8edeb928282f7f7f3a8ff60a2
Betsi Romero Fuenmayor <betsiromerof@gmail.com>
2026-08-21 18:45:39 -0500

    Configuracion inicial: React + Vite + Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+   function App() {
+     return (
+       <div className="w-screen min-h-screen flex items-center justify-center bg-gray-100">
+         <h1 className="text-4xl font-bold text-blue-600">
+           Tailwind está funcionando
+         </h1>
+       </div>
+     )
+   }
+
+   export default App
